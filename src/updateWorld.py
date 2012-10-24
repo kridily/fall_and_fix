80b4bee4dad94e711fdb566150c41b9ce5687b35
Michael Maris <ed.robot@gmail.com>
2012-10-24 15:49:52 -0400

    collisions

--- src/updateWorld.py
+++ src/updateWorld.py
@@ -10,44 +10,44 @@ from direct.interval.IntervalGlobal import * #for compound intervals
 from direct.task import Task #for update functions
 import math, sys, random, time
 from GrabBag import *
-    
+
 def keyEvents(self, task):
     """defines all keypress events"""
-    dt = task.time - self.prevTime        
-    
-    
+    dt = task.time - self.prevTime
+
+
     if self.keyMap["drop"] == 0:
         for i in range(self.pipeList.__len__()):
             self.pipeList[i].setY(self.pipeList[i].getY() - dt*100*1.6)
     if self.keyMap["drop"] == 1:
         for i in range(self.pipeList.__len__()):
             self.pipeList[i].setR(self.pipeList[i].getR() + dt*20*i)
-    
+
     dist = .1
-    if self.keyMap["moveLeft"] == 1:          
+    if self.keyMap["moveLeft"] == 1:
         self.spider.setHpr(-115, 0, 90)
         if self.spider.getX() > -2.0:
             self.spider.setX(self.spider.getX()-1*dist)
-    if self.keyMap["moveRight"] == 1: 
+    if self.keyMap["moveRight"] == 1:
         self.spider.setHpr(115, -0, -90)
         if self.spider.getX() < 2.0:
-            self.spider.setX(self.spider.getX()+1*dist)            
+            self.spider.setX(self.spider.getX()+1*dist)
     if self.keyMap["moveUp"] == 1:
         self.spider.setHpr(180, -65, 0)
         if self.spider.getZ() < 6.1:
-            self.spider.setZ(self.spider.getZ()+1*dist)            
+            self.spider.setZ(self.spider.getZ()+1*dist)
     if self.keyMap["moveDown"] == 1:
         self.spider.setHpr(180, 65, 180)
-        if self.spider.getZ() > 2.6:            
+        if self.spider.getZ() > 2.6:
             self.spider.setZ(self.spider.getZ()-1*dist)
     #print self.spider.getPos()
-    
+
     self.adjustCamera()
-    
+
     self.prevTime = task.time
     return Task.cont
-    
-        
+
+
 def adjustCamera(self):
     camvec = self.spider.getPos() - camera.getPos()
     camvec.setZ(0)
@@ -65,13 +65,13 @@ def adjustCamera(self):
     camera.setPos(camera.getPos() + (dirVec * turnRate))
     camera.setZ(4.5)
     camera.lookAt(self.spider)
-    
-    
-def loopMusic(self, task):    
+
+
+def loopMusic(self, task):
     if self.openingMusic.status() != self.openingMusic.PLAYING:
             if self.mainLoopMusic.status() != self.mainLoopMusic.PLAYING:
                 SoundInterval(self.mainLoopMusic).loop()
-                
+
     return Task.cont
 
 def checkPipes(self, task):
@@ -83,41 +83,47 @@ def checkPipes(self, task):
         self.redLightList.pop(0)
         self.redHelperList[0].removeNode()
         self.redHelperList.pop(0)
-        
+
         #remove pipe segment
         self.pipeList[0].removeNode()
         self.pipeList.pop(0)
-        
+
         #create new pipe segment
         self.createPipe(-1)
-        
+
     return Task.cont
-    
-def createPipe(self, i):                
+
+def createPipe(self, i):
         #pick file
         filename = ["../models/tunnelWallTemp"]
         filename.append(str(self.pipeBag.pick()-1))
         filename.append(".egg")
         filename = ''.join(filename)
         filename = "../models/pipe_vCurr.egg"
-        
+
         #load file
         pipe = loader.loadModel(filename)
         pipe.setScale(.0175)
         self.addPointLight(pipe)
-        
+
         #set position in queue
         if i >= 0:
             pipe.setPos(0, i*self.pipeInterval, 4.25)
         else:
             pipe.setPos(0, self.pipeList[self.pipeList.__len__()-1].getY() \
             + self.pipeInterval, 4.25)
-            
+
         #rotate by 0, 90, 180, or 270 degrees
         pipe.setR(random.randint(0,3)*90)
-        print pipe.getR()        
-        
+        print pipe.getR()
+
         pipe.reparentTo(render)
         self.pipeList.append(pipe)
-        
-        
+
+        #Adding a collision sphere to the pipe
+        cSphere = CollisionSphere((200,0,0), 100)
+        cNode = CollisionNode("pipe")
+        cNode.addSolid(cSphere)
+        cNodePath = pipe.attachNewNode(cNode)
+        cNodePath.show()
+
